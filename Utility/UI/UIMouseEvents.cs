e8bd278e50d06d162d8f06f075b77aea2865e088
Anthony Duquette <anthony-_-duquette@hotmail.com>
2018-05-30 17:40:58 -0400

    Major cleanup
    Removed unity assets, make it so all that's left are the scripts and assets to add in one's project.

---
+++ Utility/UI/UIMouseEvents.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UIMouseEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public bool OutputEvent = false;
+    public EventTypes EventType;
+
+    public UnityEvent onMouseEnter;
+    public UnityEvent onMouseOver;
+    public UnityEvent onMouseExit;
+    public UnityEvent onMouseDown;
+    public UnityEvent onMouseHeld;
+    public UnityEvent onMouseUp;
+
+    private bool m_onPointerOver = false;
+    private bool m_onMouseDown = false;
+
+    #region Enter
+    public void OnMouseEnter()
+    {
+        if (EventType == EventTypes.Mouse)
+        {
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnMouseEnter");
+            }
+
+            onMouseEnter.Invoke();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (EventType == EventTypes.Pointer)
+        {
+            m_onPointerOver = true;
+
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnPointerEnter");
+            }
+
+            onMouseEnter.Invoke();
+        }
+    }
+    #endregion
+
+    #region Over
+    public void OnMouseOver()
+    {
+        if (EventType == EventTypes.Mouse)
+        {
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnMouseOver");
+            }
+
+            onMouseOver.Invoke();
+        }
+    }
+
+    public void OnPointerOver()
+    {
+        if (EventType == EventTypes.Pointer)
+        {
+            if (m_onPointerOver)
+            {
+                if (OutputEvent)
+                {
+                    Debug.Log(transform.name + " : Update / OnPointerOver");
+                }
+
+                onMouseOver.Invoke();
+            }
+        }
+    }
+    #endregion
+
+    #region Exit
+    public void OnMouseExit()
+    {
+        if (EventType == EventTypes.Mouse)
+        {
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnMouseExit");
+            }
+
+            onMouseExit.Invoke();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (EventType == EventTypes.Pointer)
+        {
+            m_onPointerOver = false;
+
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnPointerExit");
+            }
+
+            onMouseExit.Invoke();
+        }
+    }
+    #endregion
+
+    #region Down
+    public void OnMouseDown()
+    {
+        if (EventType == EventTypes.Mouse)
+        {
+            m_onMouseDown = true;
+
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnMouseDown");
+            }
+
+            onMouseDown.Invoke();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (EventType == EventTypes.Pointer)
+        {
+            m_onMouseDown = true;
+
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnPointerDown");
+            }
+
+            onMouseDown.Invoke();
+        }
+    }
+    #endregion
+
+    #region Held
+    public void OnMouseHeld()
+    {
+        if (EventType == EventTypes.Mouse)
+        {
+            if (m_onMouseDown)
+            {
+                if (OutputEvent)
+                {
+                    Debug.Log(transform.name + " : OnMouseHeld");
+                }
+
+                onMouseHeld.Invoke();
+            }
+        }
+    }
+
+    public void OnPointerHeld()
+    {
+        if (EventType == EventTypes.Pointer)
+        {
+            if (m_onMouseDown)
+            {
+                if (OutputEvent)
+                {
+                    Debug.Log(transform.name + " : OnPointerHeld");
+                }
+
+                onMouseHeld.Invoke();
+            }
+        }
+    }
+    #endregion
+
+    #region Up
+    public void OnMouseUp()
+    {
+        if (EventType == EventTypes.Mouse)
+        {
+            m_onMouseDown = false;
+
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnMouseUp");
+            }
+
+            onMouseUp.Invoke();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (EventType == EventTypes.Pointer)
+        {
+            m_onMouseDown = false;
+
+            if (OutputEvent)
+            {
+                Debug.Log(transform.name + " : OnPointerUp");
+            }
+
+            onMouseUp.Invoke();
+        }
+    }
+    #endregion
+
+    public enum EventTypes
+    {
+        Mouse,
+        Pointer
+    }
+
+    public void Update()
+    {
+        OnPointerOver();
+        OnMouseHeld();
+        OnPointerHeld();
+    }
+}
---
+++ Utility/UI/UISizeToggle.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UISizeToggle : MonoBehaviour
+{
+    public Vector2 SizeDifference;
+    public float TransitionTime;
+
+    private RectTransform m_Transform;
+
+    private Vector2 m_FalseSize;
+    private Vector2 m_TrueSize;
+
+    private Coroutine m_CoroutineRef;
+    private bool m_State = false;
+
+    public void Initialize()
+    {
+        m_Transform = GetComponent<RectTransform>();
+
+        m_FalseSize = m_Transform.sizeDelta;
+        m_TrueSize = m_FalseSize + SizeDifference;
+    }
+
+    public void Play(bool state)
+    {
+        if (m_Transform == null)
+        {
+            Initialize();
+        }
+
+        if (m_CoroutineRef != null)
+        {
+            Stop();
+        }
+        
+        m_State = state;
+        m_CoroutineRef = StartCoroutine(PlayLoop());
+    }
+
+    public void Toggle()
+    {
+        if (m_Transform == null)
+        {
+            Initialize();
+        }
+
+        if (m_CoroutineRef != null)
+        {
+            Stop();
+        }
+        
+        m_State = !m_State;
+        m_CoroutineRef = StartCoroutine(PlayLoop());
+    }
+
+    public void Stop()
+    {
+        if (m_Transform == null)
+        {
+            Initialize();
+        }
+
+        if (m_CoroutineRef == null)
+        {
+            return;
+        }
+
+        StopCoroutine(m_CoroutineRef);
+        m_CoroutineRef = null;
+        m_Transform.sizeDelta = m_State ? m_FalseSize : m_TrueSize;
+    }
+
+    private IEnumerator PlayLoop()
+    {
+        float delta = 0.0f;
+
+        while (delta < TransitionTime)
+        {
+            delta += Time.deltaTime;
+
+            m_Transform.sizeDelta = m_State ? Vector3.Lerp(m_TrueSize, m_FalseSize, delta / TransitionTime) : Vector3.Lerp(m_FalseSize, m_TrueSize, delta / TransitionTime);
+
+            yield return null;
+        }
+    }
+}
