2c76912b6eba1f6abe848660a4d5a837141dcd63
Anthony Duquette <anthony-_-duquette@hotmail.com>
2018-06-20 13:08:47 -0400

    Update UIValueChange.cs

--- UI/UIValueChange.cs
+++ UI/UIValueChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DropdownValueChange : MonoBehaviour
+public class UIValueChange : MonoBehaviour
 {
     public Text UIText;
 
