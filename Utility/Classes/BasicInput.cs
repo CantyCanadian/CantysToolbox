90ea9c9d21eb3bf37e034471c4de74503d189b56
Anthony Duquette <anthony-_-duquette@hotmail.com>
2018-08-10 16:41:47 -0400

    Pre-Gamejam Preparation
    Added noise and string utils.
    Started adding prototyping classes.

---
+++ Utility/Classes/BasicInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Dictionary<KeyCode, UnityEvent> KeyEvent;
+    
+    private void Update()
+    {
+        foreach(KeyValuePair<KeyCode, Direction> kvp in KeyEvent)
+        {
+            if (Input.GetKey(kvp.Key))
+            {
+                kvp.Value.Invoke();
+            }
+        }
+    }
+}
---
+++ Utility/Classes/BasicMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public Dictionary<KeyCode, Direction> KeyMovement;
+    public float MovementSpeed = 3.0f;
+    
+    private void Update()
+    {
+        Vector2 movement = new Vector2();
+
+        foreach(KeyValuePair<KeyCode, Direction> kvp in KeyMovement)
+        {
+            if (Input.GetKey(kvp.Key))
+            {
+                switch(kvp.Value)
+                {
+                    case Direction.Up:
+                        movement.y += MovementSpeed * Time.deltaTime;
+                        break;
+
+                    case Direction.Down:
+                        movement.y -= MovementSpeed * Time.deltaTime;
+                        break;
+
+                    case Direction.Left:
+                        movement.x -= MovementSpeed * Time.deltaTime;
+                        break;
+
+                    case Direction.Right:
+                        movement.x += MovementSpeed * Time.deltaTime;
+                        break;
+                }
+            }
+        }
+    }
+}
