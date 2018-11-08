fd33afeb3c63097bbf4882a926ebb903f87d03ce
Anthony Duquette <anthony-_-duquette@hotmail.com>
2018-11-08 16:14:14 -0500

    Added links and removed old code.

--- UI/MouseEvents.cs
+++ UI/MouseEvents.cs
@@ -12,37 +12,48 @@ using UnityEngine.EventSystems;
 namespace Canty.UI
 {
     /// <summary>
-    /// Basic class that supports two types of mouse pointer enter, stay and exit for quick editing.
+    /// Basic wrapping class around Unity's EventTrigger object. Allows for quick binding of function on various events.
     /// </summary>
-    public class MouseEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+    public class MouseEvents : EventTrigger
     {
         public bool OutputEvent = false;
-        public EventTypes EventType;
 
-        public UnityEvent onMouseEnter;
-        public UnityEvent onMouseOver;
-        public UnityEvent onMouseExit;
-        public UnityEvent onMouseDown;
-        public UnityEvent onMouseHeld;
-        public UnityEvent onMouseUp;
+        // When clicking on and off the object.
+        public UnityEvent OnSelect;
+        public UnityEvent OnDeselect;
 
-        private bool m_onPointerOver = false;
-        private bool m_onMouseDown = false;
+        // When the selected object is updated.
+        public UnityEvent OnUpdateSelected;
 
-        #region Enter
+        // Right before a drag, whether the drag will work or not doesn't matter.
+        public UnityEvent OnInitializePotentialDrag;
 
-        public void OnMouseEnter()
-        {
-            if (EventType == EventTypes.Mouse)
-            {
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnMouseEnter");
-                }
+        // When dragging an object.
+        public UnityEvent OnBeginDrag;
+        public UnityEvent OnDrag;
+        public UnityEvent OnEndDrag;
 
-                onMouseEnter.Invoke();
-            }
-        }
+        // When dropping an object upon it.
+        public UnityEvent OnDrop;
+
+        // When using key events.
+        public UnityEvent OnSubmit;
+        public UnityEvent OnCancel;
+
+        // When clicking.
+        public UnityEvent OnPointerDown;
+        public UnityEvent OnPointerClick;
+        public UnityEvent OnPointerUp;
+
+        // When using hover.
+        public UnityEvent OnPointerEnter;
+        public UnityEvent OnPointerExit;
+
+        // When using keys to move the selection.
+        public UnityEvent OnMove;
+
+        // When the mouse wheel scrolls.
+        public UnityEvent OnScroll;
 
         public void OnPointerEnter(PointerEventData eventData)
         {
@@ -57,190 +68,6 @@ namespace Canty.UI
 
                 onMouseEnter.Invoke();
             }
-        }
-
-        #endregion
-
-        #region Over
-
-        public void OnMouseOver()
-        {
-            if (EventType == EventTypes.Mouse)
-            {
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnMouseOver");
-                }
-
-                onMouseOver.Invoke();
-            }
-        }
-
-        public void OnPointerOver()
-        {
-            if (EventType == EventTypes.Pointer)
-            {
-                if (m_onPointerOver)
-                {
-                    if (OutputEvent)
-                    {
-                        Debug.Log(transform.name + " : Update / OnPointerOver");
-                    }
-
-                    onMouseOver.Invoke();
-                }
-            }
-        }
-
-        #endregion
-
-        #region Exit
-
-        public void OnMouseExit()
-        {
-            if (EventType == EventTypes.Mouse)
-            {
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnMouseExit");
-                }
-
-                onMouseExit.Invoke();
-            }
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            if (EventType == EventTypes.Pointer)
-            {
-                m_onPointerOver = false;
-
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnPointerExit");
-                }
-
-                onMouseExit.Invoke();
-            }
-        }
-
-        #endregion
-
-        #region Down
-
-        public void OnMouseDown()
-        {
-            if (EventType == EventTypes.Mouse)
-            {
-                m_onMouseDown = true;
-
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnMouseDown");
-                }
-
-                onMouseDown.Invoke();
-            }
-        }
-
-        public void OnPointerDown(PointerEventData eventData)
-        {
-            if (EventType == EventTypes.Pointer)
-            {
-                m_onMouseDown = true;
-
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnPointerDown");
-                }
-
-                onMouseDown.Invoke();
-            }
-        }
-
-        #endregion
-
-        #region Held
-
-        public void OnMouseHeld()
-        {
-            if (EventType == EventTypes.Mouse)
-            {
-                if (m_onMouseDown)
-                {
-                    if (OutputEvent)
-                    {
-                        Debug.Log(transform.name + " : OnMouseHeld");
-                    }
-
-                    onMouseHeld.Invoke();
-                }
-            }
-        }
-
-        public void OnPointerHeld()
-        {
-            if (EventType == EventTypes.Pointer)
-            {
-                if (m_onMouseDown)
-                {
-                    if (OutputEvent)
-                    {
-                        Debug.Log(transform.name + " : OnPointerHeld");
-                    }
-
-                    onMouseHeld.Invoke();
-                }
-            }
-        }
-
-        #endregion
-
-        #region Up
-
-        public void OnMouseUp()
-        {
-            if (EventType == EventTypes.Mouse)
-            {
-                m_onMouseDown = false;
-
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnMouseUp");
-                }
-
-                onMouseUp.Invoke();
-            }
-        }
-
-        public void OnPointerUp(PointerEventData eventData)
-        {
-            if (EventType == EventTypes.Pointer)
-            {
-                m_onMouseDown = false;
-
-                if (OutputEvent)
-                {
-                    Debug.Log(transform.name + " : OnPointerUp");
-                }
-
-                onMouseUp.Invoke();
-            }
-        }
-
-        #endregion
-
-        public enum EventTypes
-        {
-            Mouse,
-            Pointer
-        }
-
-        public void Update()
-        {
-            OnPointerOver();
-            OnMouseHeld();
-            OnPointerHeld();
         }
     }
 }
