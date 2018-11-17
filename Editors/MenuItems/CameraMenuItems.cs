436586bcd4d5cb356dc0530299c80050605315a5
CantyCanadian <anthony-_-duquette@hotmail.com>
2018-11-16 21:27:26 -0500

    Compilation Bugfix 2.0
    -Also renamed the Editor utility folder to prevent the same bug.
    -Placed CameraMenuItems and TextureMerger under the Editors namespace.

--- Editors/MenuItems/CameraMenuItems.cs
+++ Editors/MenuItems/CameraMenuItems.cs
@@ -2,40 +2,43 @@
 using UnityEditor;
 using UnityEngine;
 
-public class CameraMenuItems
+namespace Canty.Editors
 {
-    /// <summary>
-    /// Replica of the default camera context menu option in order to place it in a Camera submenu in the hierarchy.
-    /// </summary>
-    [MenuItem("GameObject/Camera/Camera", false, 10)]
-    public static void CreateDefaultCamera(MenuCommand command)
+    public class CameraMenuItems
     {
-        GameObject obj = EditorUtil.CreateMenuItemGameObject(command, "Camera");
+        /// <summary>
+        /// Replica of the default camera context menu option in order to place it in a Camera submenu in the hierarchy.
+        /// </summary>
+        [MenuItem("GameObject/Camera/Camera", false, 10)]
+        public static void CreateDefaultCamera(MenuCommand command)
+        {
+            GameObject obj = EditorUtil.CreateMenuItemGameObject(command, "Camera");
 
-        obj.AddComponent<Camera>();
-        obj.AddComponent<FlareLayer>();
-        obj.AddComponent<AudioListener>();
-    }
+            obj.AddComponent<Camera>();
+            obj.AddComponent<FlareLayer>();
+            obj.AddComponent<AudioListener>();
+        }
 
-    /// <summary>
-    /// Dual camera system made to render the player character above the world in a first person setting.
-    /// </summary>
-    [MenuItem("GameObject/Camera/First-Person Camera", false, 10)]
-    public static void CreateFirstPersonCamera(MenuCommand command)
-    {
-        GameObject obj = EditorUtil.CreateMenuItemGameObject(command, "FirstPersonCamera");
+        /// <summary>
+        /// Dual camera system made to render the player character above the world in a first person setting.
+        /// </summary>
+        [MenuItem("GameObject/Camera/First-Person Camera", false, 10)]
+        public static void CreateFirstPersonCamera(MenuCommand command)
+        {
+            GameObject obj = EditorUtil.CreateMenuItemGameObject(command, "FirstPersonCamera");
 
-        obj.AddComponent<Camera>();
-        
-        GameObject localCamera = new GameObject("LocalCamera");
+            obj.AddComponent<Camera>();
 
-        localCamera.transform.SetParent(obj.transform);
+            GameObject localCamera = new GameObject("LocalCamera");
 
-        Camera camera = localCamera.AddComponent<Camera>();
+            localCamera.transform.SetParent(obj.transform);
 
-        camera.clearFlags = CameraClearFlags.Depth;
-        camera.cullingMask = 0;
+            Camera camera = localCamera.AddComponent<Camera>();
 
-        Debug.Log("First-Person Camera : To finish creating camera, set Parent Camera's Culling Mask to remove your player, while setting the Child's Culling Mask to only render the player.");
+            camera.clearFlags = CameraClearFlags.Depth;
+            camera.cullingMask = 0;
+
+            Debug.Log("First-Person Camera : To finish creating camera, set Parent Camera's Culling Mask to remove your player, while setting the Child's Culling Mask to only render the player.");
+        }
     }
 }
--- Editors/Pivot/PivotUtility.cs
+++ Editors/Pivot/PivotUtility.cs
@@ -11,7 +11,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 
-namespace Canty.Editor
+namespace Canty.Editors
 {
     public static class PivotUtilities
     {
